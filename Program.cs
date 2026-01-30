2d07e0573b5082f69070cb6dc7cf7f9f3756a478
Kodilkarshreyanshu <kodilkarshreyanshu@gmail.com>
2026-01-30 12:42:07 +0530

    docker push

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ namespace AnalyticsEngineApi
                     builder.Configuration.GetConnectionString("EmartDB")
                 )
             );
+            builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
             var app = builder.Build();
 
